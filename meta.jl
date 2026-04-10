7ee6cc11a70f55aa11fa1ff74dbd9726cf941d8d
ase22003 <ase22003@student.mdu.se>
2026-04-10 14:59:47 +0200

    Added support for blocks with multiple expressions and function declarations with returns

--- meta.jl
+++ meta.jl
@@ -5,9 +5,18 @@ Token = SubString{String}
 Tokens = Vector{Token}
 Index = UInt64
 Evaluable = Union{Expr, Symbol}
+Value = Union{Symbol, Int64, Float16, String, Char, Expr}
 
 macro token()
-	:(tokens[end]) → esc
+	quote
+		(x -> begin
+				if length(tokens) == 0
+					error("ran out of tokens")
+				end
+				return tokens[end]
+			end
+		)(nothing)
+	end → esc
 end
 macro next()
 	:(pop!(tokens)) → esc
@@ -15,31 +24,71 @@ end
 
 tokenize(str::String)::Tokens = filter!(x -> x != "", split(str, ' ')) → reverse
 
-@logged function call_expr(tokens#=::Tokens=#, EXPR_BEGIN#=::String=#, EXPR_END#=::String=#)#::Expr
+@logged function call_block(tokens::Tokens, BLOCK_BEGIN::String, BLOCK_END::String)::Expr
 	if length(tokens) == 0
-		error("there are no tokens to parse")
+		return :(nothing)
 	end
-	#if !@tokenis BEGIN
-	#	error("expression must begin with '(' -- cannot begin with '", @token, '\'')
-	#end
-	if ==(@token, EXPR_BEGIN)
+	if ==(@token, BLOCK_BEGIN)
 		@next
 	end
-	name = @token
-	args::Vector{Union{Symbol, Int64, Float16, String, Char, Expr}} = []
-	@log @token
-	@log "arguments:"
+	statements::Vector{Expr} = []
 	while true
-		@next
+		@log statements
+		@log tokens
 		if length(tokens) == 0
 			break
-		elseif ==(@token, EXPR_END)
+		elseif ==(@token, BLOCK_BEGIN)
+			push!(statements, call_block(tokens, BLOCK_BEGIN, BLOCK_END))
+		elseif ==(@token, BLOCK_END)
+			@next
 			break
-		elseif ==(@token, EXPR_BEGIN)
-			push!(args, call_expr(tokens, EXPR_BEGIN, EXPR_END))
+		elseif ==(@token, "function")
+			@log "FUNCTION"
+			@next
+			sig  = call_expr(tokens)
+			code = call_block(tokens)
+			push!(statements, Expr(:function, sig, code))
+		elseif ==(@token, "return")
+			@log "RETURN"
+			@next
+			push!(statements, Expr(:return, call_expr(tokens))) # ARG!!!!!
 		else
-			@log @token
+			@log "EXPRESSION"
+			push!(statements, call_expr(tokens)) # ARG!!!!!
+		end
+	end
+	return Expr(:block, statements...)
+end
+function call_block(tokens::Tokens)
+	call_block(tokens, "begin", "end")
+end
+
+@logged function call_expr(tokens::Tokens, EXPR_BEGIN::String, EXPR_END::String)::Expr
+	if length(tokens) == 0
+		#error("there are no tokens to parse")
+		return :(nothing)
+	end
+	if !=(@token, EXPR_BEGIN)
+		error("expression must begin with '(' -- cannot begin with '", @token, '\'')
+	end
+	@next
+	#if ==(@token, EXPR_BEGIN)
+	#	@next
+	#end
+	name = @token
+	@log name
+	@next
+	args::Vector{Value} = []
+	while true
+		@log @token
+		if     ==(@token, EXPR_BEGIN)
+			push!(args, call_expr(tokens, EXPR_BEGIN, EXPR_END))
+		elseif ==(@token, EXPR_END)
+			@next
+			break
+		else #token is a value
 			push!(args, call_value(tokens))
+			@next
 		end
 	end
 
@@ -49,7 +98,7 @@ function call_expr(tokens::Tokens)
 	call_expr(tokens, "(", ")")
 end
 
-@logged function call_value(tokens)
+@logged function call_value(tokens::Tokens)::Value
 	try
 		r = parse(Int64, @token)
 		@log "integer"
@@ -99,3 +148,5 @@ end
 
 # TODO:
 # - make a better tokenizer that can split up expressions like: "(+ 3 4)"
+# - loops, assignments, anonymous functions (->) and so on
+# - replace func(tokens, "begin", "("...) with a single dictionary of sybols func(tokens, sym_map), where sym_map = {EXPR_BEGIN => "("...}
