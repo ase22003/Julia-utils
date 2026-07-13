b9b7251a289db61c3a152d9459a61be298208277
ase22003 <ase22003@student.mdu.se>
2026-07-13 19:14:20 +0200

    fixed bug

--- math.jl
+++ math.jl
@@ -1,12 +1,12 @@
 @START_OF_DEBUG_CATEGORY "math"
 
-@logged function optimize(f::Function, params::Vector{Float64}, agression::Float64=100.0, δ::Float64=0.01, good_enough_derivative::Float64=0.1, max_time::Float64=1.0)::Vector{Float64}
+@logged function optimize(f::Function, params::Vector{Float64}, agression::Float64=0.1, δ::Float64=0.01, good_enough_derivative::Float64=0.1, max_time::Float64=1.0)::Vector{Float64}
 	derivatives = [good_enough_derivative+1 for i ∈ 1:length(params)]
 
 	start_time = time()
-	while ∃(x->x>good_enough_derivative, derivatives)
+	while ∃(x->abs(x)>good_enough_derivative, derivatives)
 		derivatives = [
-			(f(mcopy(params, i, x->x+δ)...) - f(mcopy(params, i, x->x-δ)...)) / (2δ)
+			(f(mcopy(params, i, x->x+δ)) - f(mcopy(params, i, x->x-δ))) / (2δ)
 			for i ∈ 1:length(params)
 		]
 
@@ -16,12 +16,14 @@
 		]
 
 		if time() - start_time >= max_time
+			@log "time limit reached"
 			@warn "time limit reached"
 			break
 		end
 
+		@log derivatives
 		@log params
-		@log f(params...)
+		@log f(params)
 	end
 
 	return params
